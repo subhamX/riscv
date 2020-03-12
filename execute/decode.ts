8d87b29c2d3370f50658f37d699c3d42f4529ff7
subham <subham.iitrpr@gmail.com>
2020-03-12 16:21:09 +0530

    Adds Some Functions

--- execute/decode.ts
+++ execute/decode.ts
@@ -1,11 +1,11 @@
 class RegisterFile{
     reg:number[];
-
     constructor(){
         this.reg = Array<number>(32);
         for(let i=0; i<32; i++){
             this.reg[i] = 0;
         }
+        this.reg[2] = 200000;
     }
     readValue(num:number):number{
         if(num<=31 && num>=0){
@@ -16,7 +16,8 @@ class RegisterFile{
     }
     writeValue(num:number, value:number){
         if(num<=31 && num>=0){
-            this.reg[num] = value;
+            if(num!=0)
+                this.reg[num] = value;
         }else{
             console.error("Error Encountered! Number range is not between 0-31")
         }
--- execute/main.ts
+++ execute/main.ts
@@ -1,38 +1,74 @@
 import * as fs from 'fs';
 
 
-let pc:number, instr_reg:number;
+let pc: number, instr_reg: string;
 let selectLineB, selectLineY0, selectLineY1;
-function init():void{
-    let data = fs.readFileSync(__dirname + "/test/data.m", {encoding: "utf-8"});
-    let instructions = data.split('\n');
-    // pc = instructions[]
-    console.log(instructions);
+let instructionMap: Map<number, string> = new Map<number, string>();
+function init(): void {
+    let data = fs.readFileSync(__dirname + "/test/data.m", { encoding: "utf-8" });
+    let dataArr = data.split('\n');
+    let i = 0;
+    // Loading all instructions into instructionMap
+    while (dataArr[i]) {
+        let key = parseInt(dataArr[i].split(" ")[0], 16);
+        let value = dataArr[i].split(" ")[1];
+        instructionMap.set(key, value);
+        i++;
+    }
+    // Setting pc and instr_reg
+    pc = 0;
+    // instr_reg = instructionMap.get(0);
+    // console.log(instructionMap.get(4));
 }
 
 init();
 
 
-
-
-
-function determineSelectLines(opcode: string){
+function determineSelectLines(opcode: string) {
     // If the instruction is R type then selectLineB = 0
-    if(opcode=='0110011'){
-        selectLineB=0;
-    }else{
-        selectLineB=1;
+    if (opcode == '0110011') {
+        selectLineB = 0;
+    } else {
+        selectLineB = 1;
     }
     // If the instruction is auipc then selectLineY1 = 1
-    if(opcode=="0010111"){
+    if (opcode == "0010111") {
         selectLineY1 = 1;
-    }else{
+    } else {
         selectLineY1 = 0;
     }
     // If the instruction is lw, lh, lb, ld then selectLineY0 = 1
-    if(opcode=="0000011"){
+    if (opcode == "0000011") {
         selectLineY0 = 1;
-    }else{
+    } else {
         selectLineY0 = 0;
+    }
+}
+
+
+function fetch() {
+    // Fetching the current Instruction
+    instr_reg = instructionMap.get(pc);
+
+}
+
+interface generateIAPayloadInterface {selectLineINC0, imm, selectLinePC0, returnAddress};
+
+// Function updates Program Counter
+function generateInstructionAddress(generateIAPayload:generateIAPayloadInterface) {
+    let selectLineINC0 = generateIAPayload.selectLineINC0;
+    let imm = generateIAPayload.imm;
+    let selectLinePC0 = generateIAPayload.selectLinePC0;
+    let returnAddress = generateIAPayload.returnAddress;
+    // if selectLinePC0 = 1 then new pc = returnAddress
+    if (selectLinePC0) {
+        pc = returnAddress;
+    } else {
+        // if selectLineINC0 = 1 then new pc = pc + imm
+        if (selectLineINC0) {
+            pc = pc + imm;
+        } else {
+            pc = pc + 4;
+        }
     }
 }

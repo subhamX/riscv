3d69e55671f214991ae03f645707a7ca568bdbee
subham <subham.iitrpr@gmail.com>
2020-03-12 15:37:28 +0530

    Adds Initial Files

---
+++ execute/decode.ts
@@ -0,0 +1,26 @@
+class RegisterFile{
+    reg:number[];
+
+    constructor(){
+        this.reg = Array<number>(32);
+        for(let i=0; i<32; i++){
+            this.reg[i] = 0;
+        }
+    }
+    readValue(num:number):number{
+        if(num<=31 && num>=0){
+            return this.reg[num];
+        }else{
+            console.error("Error Encountered! Number range is not between 0-31")
+        }
+    }
+    writeValue(num:number, value:number){
+        if(num<=31 && num>=0){
+            this.reg[num] = value;
+        }else{
+            console.error("Error Encountered! Number range is not between 0-31")
+        }
+    }
+}
+
+
--- execute/main.ts
+++ execute/main.ts
@@ -2,7 +2,37 @@ import * as fs from 'fs';
 
 
 let pc:number, instr_reg:number;
-
+let selectLineB, selectLineY0, selectLineY1;
 function init():void{
+    let data = fs.readFileSync(__dirname + "/test/data.m", {encoding: "utf-8"});
+    let instructions = data.split('\n');
+    // pc = instructions[]
+    console.log(instructions);
+}
 
+init();
+
+
+
+
+
+function determineSelectLines(opcode: string){
+    // If the instruction is R type then selectLineB = 0
+    if(opcode=='0110011'){
+        selectLineB=0;
+    }else{
+        selectLineB=1;
+    }
+    // If the instruction is auipc then selectLineY1 = 1
+    if(opcode=="0010111"){
+        selectLineY1 = 1;
+    }else{
+        selectLineY1 = 0;
+    }
+    // If the instruction is lw, lh, lb, ld then selectLineY0 = 1
+    if(opcode=="0000011"){
+        selectLineY0 = 1;
+    }else{
+        selectLineY0 = 0;
+    }
 }

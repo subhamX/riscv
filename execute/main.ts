1828428e0313b7bb81ebeacb8bdbed2b29c4c56c
Bh4r4t <45915454+Bh4r4t@users.noreply.github.com>
2020-03-14 21:12:56 +0530

    complete Execute(v0.1)

--- execute/main.ts
+++ execute/main.ts
@@ -1,95 +1,35 @@
 import * as fs from 'fs';
 import { addZeros } from './helperFun';
 import {RegisterFile} from './RegisterFileClass';
-import { parse } from 'querystring';
+import {opcodeMap, operationMap} from './mapPhase2';
+import {MemoryFile, addOnesZeros} from './MemoryFileClass'
 
 // PC->Program Counter, IR->Instruction Register
-let PC: number, IR: string, pcTemp: number, regFile:RegisterFile;
-let returnAddrReg;
+let PC: number, IR: string, pcTemp: number, regFile:RegisterFile, memFile:MemoryFile;
+let returnAddr;
 // intermediate Registers
-let RA, RB, RZ, RY=null, RM=null;
+let RA, RB, RZ, RY, RM=null, MAR, MDR;
 
 // instruction Type, operation Code, immediate Value
 let type, operCode, immVal;
 
 // select line for muxB, muxY
-let selectLineB, selectLineY0, selectLineY1;
+let selectLineB, selectLineY0=1, selectLineY1=0, selectLineY3=0;
 
 let instructionMap: Map<number, string> = new Map<number, string>();
-let opcodeMap: Map<string, string> = new Map<string, string>();
+let opMap: Map<string, string> = new Map<string, string>();
 let operMap : Map<string, string> = new Map<string, string>();
-// func to map opcode with corresponding instruction type
-function refMapForOpcode(opcodeMap: Map<String, string>){
-    // R type
-    opcodeMap.set('0110011', 'R');
-    // I type
-    opcodeMap.set('0010011', 'I');
-    opcodeMap.set('0000011', 'I');
-    opcodeMap.set('1100111', 'I');
-    // S type
-    opcodeMap.set('0100011', 'S');
-    // SB type
-    opcodeMap.set('1100011', 'SB');
-    // U type
-    opcodeMap.set('0110111', 'U');
-    opcodeMap.set('0010111', 'U');
-    // UJ type
-    opcodeMap.set('1101111', 'UJ');
-}
-refMapForOpcode(opcodeMap);
-
-function refMapForOperation(operMap : Map<string, string>){
-    // R type
-    operMap.set("0000000000", 'add');
-    operMap.set("0000100000", 'sub');
-    operMap.set("0000000001", 'mul');
-    operMap.set("1000000001", 'div');
-    operMap.set("1100000001", 'rem');
-    operMap.set("1110000000", 'and');
-    operMap.set("1100000000", 'or');
-    operMap.set("1000000000", 'xor');
-    operMap.set("0010000000", 'sll');
-    operMap.set("0100000000", 'slt');
-    operMap.set("1010100000", 'sra');
-    operMap.set("1010000000", 'srl');
-
-    // I type
-    operMap.set("0010011000", 'addi');
-    operMap.set("0010011111", 'andi');
-    operMap.set("0010011110", 'ori');
-    operMap.set("0000011000", 'lb');
-    operMap.set("0000011011", 'ld');
-    operMap.set("0000011001", 'lh');
-    operMap.set("0000011010", 'lw');
-    operMap.set("1100111000", 'jalr');
-
-    // S type
-    operMap.set("0100011000", 'sb');
-    operMap.set("0100011010", 'sw');
-    operMap.set("0100011011", 'sd');
-    operMap.set("0100011001", 'sh');
-
-    // SB type
-    operMap.set("1100011000", 'beq');
-    operMap.set("1100011001", 'bne');
-    operMap.set("1100011101", 'bge');
-    operMap.set("1100011100", 'blt');
-
-    // U type
-    operMap.set("0110111", 'lui');
-    operMap.set("0010111", 'auipc');
-
-    // UJ type
-    operMap.set("1101111", 'jal');
-}
-
-refMapForOperation(operMap);
+opcodeMap(opMap);
+operationMap(operMap);
 
 function init(): void {
     // let data = fs.readFileSync(__dirname + "/test/data.m", { encoding: "utf-8" });
     let data = fs.readFileSync('test/test.mc', {encoding: 'utf-8'});
     let dataArr = data.split('\n');
     let i = 0;
+    opcodeMap(opMap);
+    operationMap(operMap);
+    
     // Loading all instructions of program into instructionMap
     while (dataArr[i]) {
         let key = parseInt(dataArr[i].split(" ")[0], 16);
@@ -117,28 +57,6 @@ function init(): void {
 }
 
 init();
-
-
-function determineSelectLines(opcode: string) {
-    // If the instruction is R type then selectLineB = 0
-    if (opcode == '0110011') {
-        selectLineB = 0;
-    } else {
-        selectLineB = 1;
-    }
-    // If the instruction is auipc then selectLineY1 = 1
-    if (opcode == "0010111") {
-        selectLineY1 = 1;
-    } else {
-        selectLineY1 = 0;
-    }
-    // If the instruction is lw, lh, lb, ld then selectLineY0 = 1
-    if (opcode == "0000011") {
-        selectLineY0 = 1;
-    } else {
-        selectLineY0 = 0;
-    }
-}
 
 // Stage 1
 // program counter pc register which holds the address of the current instruction
@@ -168,21 +86,20 @@ function Decode() {
     // extracting opcode of current instruction
     let opcode = IR.slice(25, 32);
     // instruction type of current instruction
-    let type = opcodeMap.get(opcode);
-    let immVal :string = "0";
-    let operCode;
+    let type = opMap.get(opcode);
+    immVal = "0";
     if(type == 'R'){
         // operation code
         operCode = IR.slice(17 ,20) + IR.slice(0, 7);
 
         let rd = IR.slice(20, 25);
-        regFile.setRD(rd);
+        regFile.setRD(parseInt(rd, 2));
 
         let rs1 = IR.slice(12, 17);
-        regFile.setRS1(rs1);
+        regFile.setRS1(parseInt(rs1, 2));
 
         let rs2 = IR.slice(7 ,12)
-        regFile.setRS2(rs2);
+        regFile.setRS2(parseInt(rs2, 2));
         selectLineB = 0;
     }
     else if(type == 'I'){
@@ -190,28 +107,28 @@ function Decode() {
         operCode = opcode + IR.slice(17, 20);
 
         let rd = IR.slice(20, 25);
-        regFile.setRD(rd);
+        regFile.setRD(parseInt(rd, 2));
 
         let rs1 = IR.slice(12, 17);
-        regFile.setRS1(rs1);
+        regFile.setRS1(parseInt(rs1, 2));
 
         // rs2 has the second operand
 
-        let immVal = IR.slice(0, 12);
+        immVal = IR.slice(0, 12);
     }
     else if(type == 'S'){
         // operation code
         operCode = opcode + IR.slice(17, 20);
 
         let rs1 = IR.slice(12, 17);
-        regFile.setRS1(rs1);
+        regFile.setRS1(parseInt(rs1, 2));
 
         let rs2 = IR.slice(7 ,12)
-        regFile.setRS2(rs2);
+        regFile.setRS2(parseInt(rs2, 2));
 
         let imm4_0 = IR.slice(20, 25);
         let imm11_5 = IR.slice(0, 7);
-        let immVal = imm11_5 + imm4_0;
+        immVal = imm11_5 + imm4_0;
 
         // rs2 has the base address
         selectLineB = 0
@@ -221,12 +138,12 @@ function Decode() {
         operCode = opcode + IR.slice(17, 20);
 
         let rs1 = IR.slice(12, 17);
-        regFile.setRS1(rs1);
+        regFile.setRS1(parseInt(rs1, 2));
 
         let rs2 = IR.slice(7 ,12)
-        regFile.setRS2(rs2);
+        regFile.setRS2(parseInt(rs2, 2));
 
-        let immVal = IR.slice(0) + IR.slice(25) + IR.slice(1, 7) + IR.slice(20, 24);
+        immVal = IR.slice(0) + IR.slice(25) + IR.slice(1, 7) + IR.slice(20, 24);
         
         // rs2 has the base address
         selectLineB = 0
@@ -236,134 +153,120 @@ function Decode() {
         operCode = opcode;
 
         let rd = IR.slice(20, 25);
-        regFile.setRD(rd);
+        regFile.setRD(parseInt(rd, 2));
 
-        let immVal = IR.slice(0, 20);
+        immVal = IR.slice(0, 20);
     }
     else if(type == 'UJ'){
         // operation code
         operCode = opcode;
 
         let rd = IR.slice(20, 25);
-        regFile.setRD(rd);
+        regFile.setRD(parseInt(rd, 2));
 
-        let immVal = IR.slice(0) + IR.slice(1, 9) + IR.slice(9) + IR.slice(10, 20);
+        immVal = IR.slice(0) + IR.slice(1, 9) + IR.slice(9) + IR.slice(10, 20);
     }
     else{
         console.error('Not a valid instruction!(invalid Opcode)');
     }
 }
 
-
-interface generateIAPayloadInterface { selectLineINC0, imm, selectLinePC0, returnAddress };
-
-// Function updates Program Counter
-function generateInstructionAddress(generateIAPayload: generateIAPayloadInterface) {
-    let selectLineINC0 = generateIAPayload.selectLineINC0;
-    let imm = generateIAPayload.imm;
-    let selectLinePC0 = generateIAPayload.selectLinePC0;
-    let returnAddress = generateIAPayload.returnAddress;
-    // if selectLinePC0 = 1 then new pc = returnAddress
-    if (selectLinePC0) {
-        PC = returnAddress;
-    } else {
-        // if selectLineINC0 = 1 then new pc = pc + imm
-        if (selectLineINC0) {
-            PC = PC + imm;
-        } else {
-            PC = PC + 4;
+//  updates PC
+function UpdatePC(PC_Select: number, inpImm?:number) : void{
+    pcTemp = PC;
+    PC-=4;
+    if(PC_Select == 0){
+        PC = RZ;
+    }
+    else{
+        if(inpImm){
+            PC+=immVal;
+        }
+        else{
+            PC+=4;
         }
     }
 }
 
+// evaluate Immediate values
+function evaluateImm(imVal : string) : number{
+    imVal = addOnesZeros(imVal);
+    return parseInt(imVal, 2);
+}
+
 // Stage - 3
 function Execute(instType : string, operCode : string, immVal : string){
-    let inA = parseInt(regFile.getRS1(), 16);
-    let inB;
+    let inA :number = regFile.getRS1();
+    let inB :number;
     if(selectLineB){
-        inB = parseInt(immVal, 16);
+        inB = evaluateImm(immVal);
     }
     else{
-        inB = regFile.getRS2();
+        inB = regFile.getRS2()
     }
-    inB = parseInt(inB, 16);
     // finding operation to be performed
     let ALU_op = operMap.get(operCode);
 
     if(ALU_op == ('add' || 'addi' || 'ld' || 'lb' || 'lh' || 'lw')){
         RZ = inA + inB;
-        RZ = parseInt(RZ, 10).toString(16);
     }
     else if(ALU_op == 'sub'){
         RZ = inA - inB;
-        RZ = parseInt(RZ, 10).toString(16);
     }
     else if(ALU_op == 'div'){
-        // TODO: current output is decimal, required is integer
-        RZ = inA/inB;
-        RZ = parseInt(RZ, 10).toString(16);
+        RZ = Math.floor(inA/inB);
     }
     else if(ALU_op == 'rem'){
         RZ = inA%inB;
-        RZ = parseInt(RZ, 10).toString(16);
     }
     else if(ALU_op == 'mul'){
         RZ = inA*inB;
-        RZ = parseInt(RZ, 10).toString(16);
     }
     else if(ALU_op == ('andi' || 'and)')){
         RZ = inA&inB;
-        RZ = parseInt(RZ, 10).toString(16);
     }
     else if(ALU_op == ('ori' || 'or')){
         RZ = inA|inB;
-        RZ = parseInt(RZ, 10).toString(16);
     }
     else if(ALU_op == 'xor'){
         RZ = inA^inB;
-        RZ = parseInt(RZ, 10).toString(16);
     }
     else if(ALU_op == 'beq'){
         if(inA == inB){
-            // TODO : update the PC
+            UpdatePC(evaluateImm(immVal));
         }
     }
     else if(ALU_op == 'bne'){
         if(inA != inB){
-            // TODO : update the PC
+            UpdatePC(evaluateImm(immVal));
         }
     }
     else if(ALU_op == 'bge'){
         if(inA >= inB){
-            // TODO : update the PC
+            UpdatePC(evaluateImm(immVal));
         }
     }
     else if(ALU_op == 'blt'){
         if(inA < inB){
-            // TODO : update the PC
+            UpdatePC(evaluateImm(immVal));
         }
     }
     else if(ALU_op == 'sll'){
         RZ = inA << inB;
-        RZ = parseInt(RZ, 10).toString(16);
     }
     else if(ALU_op == 'slt'){
         RZ = (inA < inB) ? 1:0;
-        RZ = parseInt(RZ, 10).toString(16);
     }
     else if(ALU_op == 'sra'){
         RZ = inA >> inB;
         RZ |= inA & (1<<31);
-        RZ = parseInt(RZ, 10).toString(16);
     }
     else if(ALU_op == 'srl'){
         RZ = inA >> inB;
-        RZ = parseInt(RZ, 10).toString(16);
     }
     else if(ALU_op == 'jalr'){
-        // RZ = inA
-        // TODO: update the PC
-        // update the return address
+        PC = returnAddr;
+        
     }
     else if(ALU_op == ('sb' || 'sw' || 'sd' || 'sh')){
         RZ = inA + immVal;
@@ -376,16 +279,40 @@ function Execute(instType : string, operCode : string, immVal : string){
         // TODO:auipc
     }
     else if(ALU_op == 'jal'){
+        returnAddr = PC;
+        UpdatePC(evaluateImm(immVal), 1);
         // TODO:jal
     }
 }
 
 // Stage - 4
 function MemoryOperations(){
-    // if(selectLineY0)
+    if(type == 'S'){
+        selectLineY0 = 0;
+        selectLineY1 = 0;
+        memFile.MEM_WRITE(RZ, RM, operMap.get(operCode).slice(1));
+    }
+    else if(operMap.get(operCode) == ('ld'||'lw'||'lh'||'lb')){
+        selectLineY0 = 0;
+        selectLineY1 = 1;
+        MDR = memFile.MEM_READ(RZ, operMap.get(operCode).slice(1));
+    }
 }
 
 // Stage - 5
 function WriteBack(){
+    if(selectLineY0){
+        RY = RZ;
+    }
+    else if(selectLineY1){
+        RY = MDR;
+    }
+    else if(selectLineY3){
+        RY = returnAddr;
+    }
+    else{
+        console.error("Something's Wrong with the select line for muxY");
+        return;
+    }
 
 }

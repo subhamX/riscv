a7427a017939a3bbc4bb1dc0115fd93cfacbf59f
Bh4r4t <45915454+Bh4r4t@users.noreply.github.com>
2020-03-15 05:20:48 +0530

    Now it works fine

--- execute/MemoryFileClass.ts
+++ execute/MemoryFileClass.ts
@@ -1,15 +1,15 @@
 export class MemoryFile{
     private memory : Array<number>;
     private UpAddr : number;
-    stackStart: number = parseInt('0x7FFFFFEC', 16);
-    heapStart: number = parseInt('0x10008000', 16);
-    dataStart: number = parseInt('0x10000000', 16);
-    textStart: number = 0;
+    // stackStart: number = parseInt('0x7FFFFFEC', 16);
+    // heapStart: number = parseInt('0x10008000', 16);
+    // dataStart: number = parseInt('0x10000000', 16);
+    // textStart: number = 0;
 
     constructor(){
-        this.memory = Array<number>(2147483644);
-        this.UpAddr = 2147483644;
-        for(let i=0;i<2147483644;i++){
+        this.memory = Array<number>(2000000);
+        this.UpAddr = 2000000;
+        for(let i=0;i<2000000;i++){
             this.memory[i]=0;
         }
     }
@@ -23,8 +23,9 @@ export class MemoryFile{
         else if(dtype == 'd'){len = 8;}
         if(addr<(this.UpAddr+len) && addr>=0){
             for(let i=0;i<len;i++){
-                temp = this.memory[addr] + temp;
-                addr +=1;
+                // TODO: check for byte and word i.e. length of tostring output
+                temp = (this.memory[addr+i]).toString(2) + temp;
+                // console.log((this.memory[addr+i]).toString(2));
             }
             return parseInt(addOnesZeros(temp), 2)>>0;
         }
@@ -34,17 +35,23 @@ export class MemoryFile{
     }
 
     MEM_WRITE(addr: number, value:number, dtype:string){
-        // TODO: convert negative numbers to binary string
         let len : number;
         if(dtype == 'b'){len = 1;}
         else if(dtype == 'h'){len = 2;}
         else if(dtype == 'w'){len = 4;}
         else if(dtype == 'd'){len = 8;}
         if(addr>=0 && addr < this.UpAddr){
-            let tempStr : number;
+            let valString : string, tempStr : string;
+            if(value>=0){
+                valString = addZeros(value.toString(2), 8*len);
+            }
+            else{
+                valString = (value >>> 0).toString(2);
+            }
+            // console.log(valString);
             for(let i=0;i<len;i++){
-                tempStr = value<<((len*8)-(i*8));
-                this.memory[addr+i] = tempStr>>(len*8);
+                tempStr = valString.slice((len-i-1)*8+1, (len-i)*8);
+                this.memory[addr + i] = parseInt(tempStr, 2);
             }
         }
         else{
@@ -63,4 +70,10 @@ export function addOnesZeros(value: string) :string {
         pad = "0".repeat(32-value.length);
     }
     return pad+value;
+}
+
+export function addZeros(imm:string, length: number):string{
+    let n = length - imm.length;
+    let immPad = "0".repeat(n);
+    return immPad + imm;
 }
